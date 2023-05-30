31f5982c0730544e85c49afa9fad007a30789efe
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-31 01:44:00 +0600

    added to navber component

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navber from "../Pages/Shared/Navber/Navber";
+import Footer from "../Pages/Shared/Footer/Footer";
 
 const Main = () => {
     return (
         <div>
+            <Navber></Navber>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
