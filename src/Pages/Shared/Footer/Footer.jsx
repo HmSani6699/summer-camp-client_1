632bdafb332dc1b35c370e572b799950a220b78b
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-31 02:26:25 +0600

    creat and style update in footer

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,9 +1,30 @@
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer>
+            <div className="footer  bg-neutral text-neutral-content gap-0">
+                <div className=" w-full h-full py-10 flex  text-center bg-[#1F2937] items-center justify-center">
+                    <div className="text-white space-y-2">
+                        <h2 className="text-2xl font-bold">CONTACT US</h2>
+                        <p>123 ABS Street, Uni 21, Bangladesh</p>
+                        <p>+88 123456789</p>
+                        <p>Mon - Fri: 08:00 - 22:00</p>
+                        <p>Sat - Sun: 10:00 - 23:00</p>
+                    </div>
+                </div>
+                <div className=" w-full h-full py-10 flex  text-center bg-[#111827] items-center justify-center">
+                    <div className="text-white space-y-2">
+                        <h2 className="text-2xl font-bold">Follow US</h2>
+                        <p>Join us on social media</p>
+                    </div>
+                </div>
+            </div>
+            <div className="footer footer-center p-4 bg-[#151515] text-white">
+                <div>
+                    <p>Copyright © CulinaryCloud. All rights reserved.</p>
+                </div>
+            </div>
+        </footer>
     );
 };
 
--- src/Pages/Shared/Navber/Navber.jsx
+++ src/Pages/Shared/Navber/Navber.jsx
@@ -2,14 +2,16 @@
 const Navber = () => {
 
     const navLink = <>
-        <li><a>Item 1</a></li>
-        <li><a>Item 3</a></li>
+        <li><a>Home</a></li>
+        <li><a>DASHBOARD</a></li>
+        <li><a>Our Menu</a></li>
+        <li><a>Our Shop</a></li>
     </>
 
     return (
         <div>
             <div className="navbar bg-base-100">
-                <div className="navbar-start">
+                <div className="navbar-start ">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -18,15 +20,15 @@ const Navber = () => {
                             {navLink}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    <span><h2 className="font-bold text-2xl">BISTRO BOSS</h2><p className="">Restaurant</p></span>
                 </div>
-                <div className="navbar-end">
+                <div className="navbar-end  w-full">
                     <div className="navbar-end hidden lg:flex">
                         <ul className="menu menu-horizontal px-1">
                             {navLink}
                         </ul>
                     </div>
-                    <a className="btn">Get started</a>
+                    <a className="btn">Login</a>
                 </div>
             </div>
         </div>
