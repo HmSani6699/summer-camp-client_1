4b6c7cfd2565116f9f8f31e41f222fd3c1c757da
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-31 23:42:49 +0600

    added to review data in pablic folder

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -5,6 +5,7 @@ import Category from "../Category/Category";
 import Featured from "../Featured/Featured";
 import FoodCards from "../FoodCards/FoodCards";
 import Manubanner from "../ManuBanner/Manubanner";
+import Testimonials from "../Testimonials/Testimonials";
 
 const Home = () => {
     return (
@@ -16,6 +17,7 @@ const Home = () => {
             <ContactUs></ContactUs>
             <FoodCards></FoodCards>
             <Featured></Featured>
+            <Testimonials></Testimonials>
         </div>
     );
 };
