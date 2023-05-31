d9ef4e9d427c3f2f4f4d2f7dc884a7f1e09b1858
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-01 00:35:57 +0600

    added to react rating

--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -3,25 +3,54 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper";
+import { useEffect, useState } from "react";
+import { Rating } from "@smastrom/react-rating";
+import '@smastrom/react-rating/style.css'
+import { FaQuoteLeft } from "react-icons/fa";
+
+
 
 const Testimonials = () => {
+    const [reviews, setReview] = useState();
+
+    useEffect(() => {
+        fetch('review.json')
+            .then(res => res.json())
+            .then(data => {
+                setReview(data)
+            })
+    }, []);
+
+    console.log(reviews);
     return (
-        <section>
+        <section className="max-w-6xl mx-auto mb-28">
             <SectionTitle
                 hedding={'TESTIMONIALS'}
                 subHadding={'---What Our Clients Say---'}
             ></SectionTitle>
 
-            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide>
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper mt-10">
+
+                {
+                    reviews?.map(review => <SwiperSlide
+                        key={review._id}
+                    >
+                        <div className="px-20  text-center">
+                            <div className="justify-center items-center flex mb-6">
+                                <Rating
+                                    style={{ maxWidth: 180 }}
+                                    value={review.rating}
+                                    readOnly
+                                />
+                            </div>
+                            <div className="justify-center items-center flex ">
+                                <FaQuoteLeft className="text-7xl font-bold mb-8"></FaQuoteLeft>
+                            </div>
+                            <p>{review.details}</p>
+                            <h3 className="mt-6 text-3xl font-bold text-[#CD9003]">{review.name}</h3>
+                        </div>
+                    </SwiperSlide>)
+                }
             </Swiper>
         </section>
     );
