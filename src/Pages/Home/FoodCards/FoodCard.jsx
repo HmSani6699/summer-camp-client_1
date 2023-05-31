166513af881df9bc7d9b11c96cb83d2b9b132158
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-31 22:19:31 +0600

    update button style

--- src/Pages/Home/FoodCards/FoodCard.jsx
+++ src/Pages/Home/FoodCards/FoodCard.jsx
@@ -8,7 +8,7 @@ const FoodCard = ({ item }) => {
                 <h2 className="card-title text-center">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end mt-3">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <button className="btn bg-[#E8E8E8] text-[#BB8506] border-0 border-b-4 border-[#BB8506]">add to cart</button>
                 </div>
             </div>
         </div>
