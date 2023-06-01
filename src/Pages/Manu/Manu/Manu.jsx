374779122f88eed783362351fefe170f767d092e
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-01 09:34:52 +0600

    added to view button

--- src/Pages/Manu/Manu/Manu.jsx
+++ src/Pages/Manu/Manu/Manu.jsx
@@ -2,11 +2,12 @@ import { Helmet } from 'react-helmet';
 import ManuCover from '../../Shared/ManuCover/ManuCover';
 import PopularManu from '../../PopularManu/PopularManu';
 
-import coberImg1 from '../../../assets/menu/dessert-bg.jpeg';
+import coberImg1 from '../../../assets/menu/banner3.jpg';
 import coberImg2 from '../../../assets/menu/dessert-bg.jpeg';
 import coberImg3 from '../../../assets/menu/pizza-bg.jpg';
 import coberImg4 from '../../../assets/menu/salad-bg.jpg';
 import coberImg5 from '../../../assets/menu/soup-bg.jpg';
+import SectionTitle from '../../../component/SectionTitle/SectionTitle';
 
 
 
@@ -14,41 +15,74 @@ const Manu = () => {
     return (
         <div>
             <Helmet><title>Bistro boss | Manu</title></Helmet>
-
+            {/* Main  Manu */}
             <ManuCover
                 coberImg={coberImg1}
                 title='OUR MENU'
                 description='Would you like to try a dish'
             ></ManuCover>
-            <PopularManu></PopularManu>
+            <SectionTitle
+                hedding={'TODAYS OFFER'}
+                subHadding={'---Do not miss---'}
+            ></SectionTitle>
+            <div className='my-24'>
+                <PopularManu></PopularManu>
+                <div className='flex justify-center'>
+                    <button className="btn bg-[#E8E8E8] text-[#BB8506] border-0 border-b-4 border-[#BB8506]">ORDER YOUR FAVOURITE FOOD</button>
+                </div>
+            </div>
 
+            {/* Desserts  Manu */}
             <ManuCover
                 coberImg={coberImg2}
                 title='Desserts'
                 description='Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
             ></ManuCover>
-            <PopularManu></PopularManu>
+            <div className='my-24'>
+                <PopularManu></PopularManu>
+                <div className='flex justify-center'>
+                    <button className="btn bg-[#E8E8E8] text-[#BB8506] border-0 border-b-4 border-[#BB8506]">ORDER YOUR FAVOURITE FOOD</button>
+                </div>
+            </div>
 
+            {/* Pizza  Manu */}
             <ManuCover
                 coberImg={coberImg3}
                 title='pizza'
                 description='Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
             ></ManuCover>
-            <PopularManu></PopularManu>
+            <div className='my-24'>
+                <PopularManu></PopularManu>
+                <div className='flex justify-center'>
+                    <button className="btn bg-[#E8E8E8] text-[#BB8506] border-0 border-b-4 border-[#BB8506]">ORDER YOUR FAVOURITE FOOD</button>
+                </div>
+            </div>
 
+            {/* Salads  Manu */}
             <ManuCover
                 coberImg={coberImg4}
                 title='salads'
                 description='Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
             ></ManuCover>
-            <PopularManu></PopularManu>
+            <div className='my-24'>
+                <PopularManu></PopularManu>
+                <div className='flex justify-center'>
+                    <button className="btn bg-[#E8E8E8] text-[#BB8506] border-0 border-b-4 border-[#BB8506]">ORDER YOUR FAVOURITE FOOD</button>
+                </div>
+            </div>
 
+            {/* Soups  Manu */}
             <ManuCover
                 coberImg={coberImg5}
                 title='soups'
                 description='Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
             ></ManuCover>
-            <PopularManu></PopularManu>
+            <div className='my-24'>
+                <PopularManu></PopularManu>
+                <div className='flex justify-center'>
+                    <button className="btn bg-[#E8E8E8] text-[#BB8506] border-0 border-b-4 border-[#BB8506]">ORDER YOUR FAVOURITE FOOD</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/ManuCover/ManuCover.jsx
+++ src/Pages/Shared/ManuCover/ManuCover.jsx
@@ -2,14 +2,13 @@
 const ManuCover = ({coberImg,title,description}) => {
     return (
         <section>
-            <div className="hero h-[600px] mb-20 max-w-6xl mx-auto" style={{ backgroundImage: `url(${coberImg})` }}>
+            <div className="hero h-[600px] mb-32" style={{ backgroundImage: `url(${coberImg})` }}>
                 <div className="hero-content text-center  text-neutral-content  bg-[#00000080] w-4/6">
                     <div className="max-w-md text-white py-6">
                         <h1 className="mb-5 text-5xl font-bold">{title}</h1>
                         <p className="mb-5 ">{description}</p>
                     </div>
                 </div>
-
             </div>
         </section>
     );
