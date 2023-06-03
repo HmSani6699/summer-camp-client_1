650da4222cdc29ea84572e796c1fbdda5a3acd69
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-03 09:53:38 +0600

    update the ten stack query

--- src/hooks/useCards.jsx
+++ src/hooks/useCards.jsx
@@ -5,13 +5,14 @@ import { useQuery } from "@tanstack/react-query"
 const useCards = () => {
     const { user } = useContext(AuthContext)
 
-    const { data:cart, refetch } = useQuery({
-        queryKey: ['carts', user?.email],
-        queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`);
-          return res.json()
+    const { data:cart,refetch } = useQuery({
+        queryKey: ['carts',user?.email],
+        queryFn: async () =>{
+            const res=await fetch(`http://localhost:5000/carts?email=${user?.email}`);
+            return res.json()
         }
-    })
+         
+      })
 
     return [cart, refetch]
 }
