9fb00fcc5a6ffb3ad488ec7a33d499803e7d739c
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-03 08:31:22 +0600

    ser up to ten stack query

--- src/hooks/useCards.jsx
+++ src/hooks/useCards.jsx
@@ -1,9 +1,18 @@
 import { useContext } from "react"
 import { AuthContext } from "../Providers/AuthProvider"
+import { useQuery } from "@tanstack/react-query"
 
 const useCards=()=>{
     const{user}=useContext(AuthContext)
+    const { data:cart } = useQuery({
+        queryKey: ['films'],
+        queryFn: async () =>{
+            const res= await fetch(`http://localhost:5000/cards?email=${user?.email}`);
+            res.send(res)
+        }
+      })
 
+      return[cart]
 }
 
 export default useCards
