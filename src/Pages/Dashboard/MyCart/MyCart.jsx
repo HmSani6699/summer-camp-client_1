4a53274fb40684dd38de8adb78b59497ff0e8123
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-03 15:39:19 +0600

    added the oarder item in maycart

--- src/Pages/Dashboard/MyCart/MyCart.jsx
+++ src/Pages/Dashboard/MyCart/MyCart.jsx
@@ -1,3 +1,4 @@
+import SectionTitle from "../../../component/SectionTitle/SectionTitle";
 import useCards from "../../../hooks/useCards";
 
 const MyCart = () => {
@@ -5,10 +6,18 @@ const MyCart = () => {
     const [cart] = useCards()
     console.log(cart);
 
-    
+
     return (
         <div>
-            <h2>this is may cart</h2>
+            <SectionTitle hedding={'WANNA ADD MORE?'} subHadding={'---My Cart---'}></SectionTitle>
+
+            <div className=" p-10 w-full bg-base-100 shadow-xl border">
+                <div className="lg:flex justify-between gap-5">
+                    <h2 className="uppercase">Total orders: {cart?.length}</h2>
+                    <h2 className="uppercase">total price: ${cart?.length}</h2>
+                    <button className="btn btn-sm border-0 bg-[#BB8506]">PAY</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/component/SectionTitle/SectionTitle.jsx
+++ src/component/SectionTitle/SectionTitle.jsx
@@ -1,10 +1,10 @@
 
 const SectionTitle = ({ hedding, subHadding ,color}) => {
     return (
-        <div className="text-center mt-20 mb-12">
+        <div className="text-center mt-20 mb-12 flex items-center justify-center">
             <div>
             <p className="text-[#D99904]">{subHadding}</p>
-            <h2 className={`text-3xl py-2 mt-2 mx-auto w-1/4  border-b-4 border-t-4 ${color==='featured'?'text-white':'text-black'}`}>{hedding}</h2>
+            <h2 className={`text-3xl py-2 mt-2 mx-auto   border-b-4 border-t-4 ${color==='featured'?'text-white':'text-black'}`}>{hedding}</h2>
             </div>
         </div>
     );
