ea1e3fdc50a8b163890bad920fe8d0d346be20f2
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-06 16:08:14 +0600

    update the jwt post code

--- src/hooks/useCards.jsx
+++ src/hooks/useCards.jsx
@@ -4,21 +4,23 @@ import { useQuery } from "@tanstack/react-query"
 
 const useCards = () => {
     const { user } = useContext(AuthContext)
+    // console.log(user);
 
     const token = localStorage.getItem('access-token')
 
-    const { data:cart,refetch } = useQuery({
-        queryKey: ['carts',user?.email],
-        queryFn: async () =>{
-            const res=await fetch(`http://localhost:5000/carts?email=${user?.email}`,{
-                headers:{
-                    authorization:`bearer ${token}`
+    const { data: cart, refetch } = useQuery({
+        queryKey: ['carts', user?.email],
+        enabled:!!user?.email,
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`, {
+                headers: {
+                    authorization: `bearer ${token}`
                 }
             });
             return res.json()
-        }
-         
-      })
+        },
+
+    })
 
     return [cart, refetch]
 }
